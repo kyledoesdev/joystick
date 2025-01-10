f58c3de2f79ee4c6a9f26d1b5cddcdbacf205022
Shania Robertson <shania.robertson23@gmail.com>
2025-01-09 19:37:06 -0500

    landing page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,12 +17,17 @@
     </head>
     <body>
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
-            <div class="text-4xl">
-                🕹️
+            <div class="text-5xl flex flex-row">
+                <div>
+                    🕹️
+                </div>
+                <div class="bg-gradient-to-b from-red-500 to-indigo-900 text-transparent bg-clip-text pb-1">
+                    Joystick Jury
+                </div>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4hadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
+               {{ $slot }}
             </div>
         </div>
 
