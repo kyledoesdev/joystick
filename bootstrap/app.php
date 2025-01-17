a94cb667e05752fa32bb90db0c914747e2dd78be
kyledoesdev <kyledoesdev@gmail.com>
2025-01-16 20:46:41 -0500

    create action stub and command added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(fn (Request $request) => route('login'));
     })
+    ->withCommands([
+        App\Console\Commands\CreateAction::class
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
