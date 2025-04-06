a938aacb4af0cf7294001c3135d19c3f39632319
kyledoesdev <kyledoesdev@gmail.com>
2025-04-06 17:57:39 -0400

    add health check and developer middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,14 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use SocialiteProviders\Manager\SocialiteWasCalled;
 use SocialiteProviders\Twitch\Provider;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
+use Spatie\Health\Checks\Checks\DebugModeCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+use Spatie\Health\Facades\Health;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,6 +39,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         $this->bootMacros();
+        $this->registerHealthChecks();
     }
 
     private function bootMacros(): void
@@ -44,5 +53,22 @@ class AppServiceProvider extends ServiceProvider
                 Str::endsWith($string, ['s', 'S']) ? '' : 's'
             );
         });
+    }
+
+    private function registerHealthChecks(): void
+    {
+        Health::checks([
+            EnvironmentCheck::new(),
+            DatabaseCheck::new(),
+            DatabaseConnectionCountCheck::new()
+                ->failWhenMoreConnectionsThan(100),
+            DebugModeCheck::new(),
+            OptimizedAppCheck::new(),
+            ScheduleCheck::new()
+                ->heartbeatMaxAgeInMinutes(15),
+            UsedDiskSpaceCheck::new()
+                ->warnWhenUsedSpaceIsAbovePercentage(90)
+                ->failWhenUsedSpaceIsAbovePercentage(95)
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,15 @@
 
 use App\Http\Controllers\SocialiteController;
 use App\Http\Middleware\GroupMiddleware;
+use App\Http\Middleware\IsDeveloper;
 use App\Livewire\Dashboard;
-use App\Livewire\Group\ShowGroup;
 use App\Livewire\Group\EditGroup;
 use App\Livewire\Group\GroupFeeds;
+use App\Livewire\Group\ShowGroup;
 use App\Livewire\Invites\Index;
 use App\Livewire\Suggestions\Show as Suggestions;
 use Illuminate\Support\Facades\Route;
+use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
 Route::get('/', fn () => auth()->check() ? redirect(route('dashboard')) : view('welcome'))->name('login');
 
@@ -27,4 +29,8 @@ Route::middleware(['auth'])->group(function() {
     });
 
     Route::get('/invites', Index::class)->name('invites');
+
+    Route::middleware(IsDeveloper::class)->group(function() {
+        Route::get('health', HealthCheckResultsController::class);
+    });
 });
